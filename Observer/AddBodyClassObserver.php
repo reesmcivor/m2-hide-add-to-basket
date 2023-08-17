f7889c437db04993cfc5ce1f531d1835dd82df22
reesmcivor <reesmcivor@hotmail.com>
2023-08-17 15:58:20 +0100

    Hide add to basket

---
+++ Observer/AddBodyClassObserver.php
@@ -0,0 +1,31 @@
+<?php
+namespace ReesMcIvor\HideAddToBasket\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+
+class AddBodyClassObserver implements ObserverInterface
+{
+    protected $registry;
+    protected $pageConfig;
+
+    public function __construct(
+        \Magento\Framework\Registry         $registry,
+        \Magento\Framework\View\Page\Config $pageConfig
+    )
+    {
+        $this->registry = $registry;
+        $this->pageConfig = $pageConfig;
+    }
+
+    public function execute(Observer $observer)
+    {
+        $product = $this->registry->registry('current_product');
+        if ($product) {
+            $hideAddToCart = $product->getCustomAttribute('hide_addtocart');
+            if ($hideAddToCart && $hideAddToCart->getValue() == '1') {
+                $this->pageConfig->addBodyClass('hide-add-to-cart');
+            }
+        }
+    }
+}
